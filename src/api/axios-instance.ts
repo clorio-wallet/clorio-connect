daf4460b5a7c98debe322f5252dc499af526dab5
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2026-01-22 15:00:00 +0100

    chore: externalize API URL to environment variables

---
+++ src/api/axios-instance.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export const AXIOS_INSTANCE = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+    'Content-Type': 'application/json',
+  },
+});
+
+export const customInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig,
+): Promise<T> => {
+  const source = axios.CancelToken.source();
+  const promise = AXIOS_INSTANCE({
+    ...config,
+    ...options,
+    cancelToken: source.token,
+  }).then(({ data }) => data);
+
+  // @ts-ignore
+  promise.cancel = () => {
+    source.cancel('Query was cancelled');
+  };
+
+  return promise;
+};
